1ee48e884a2d78b4e59119df366821f2454953ad
Mike Williams <mumakin@gmail.com>
2019-08-18 16:44:40 -0600

    accumulate newline funciton works

--- chapter02/chapter02.cpp
+++ chapter02/chapter02.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -7,7 +8,38 @@
 #define CATCH_CONFIG_MAIN 
 #include "catch2/catch.hpp"
 
-TEST_CASE("NewLineInString")
+int CountIfNewline(int previousCount, char currentCharacter)
 {
-	REQUIRE(false);
+	if('\n' != currentCharacter)
+	{
+		return previousCount;
+	}
+	return previousCount + 1;
+}
+
+int LinesInString_Accumulate(const std::string input)
+{
+	return std::accumulate(input.cbegin(), input.cend(), 0, CountIfNewline);
+}
+
+TEST_CASE("CountIfNewline")
+{
+	REQUIRE(5 == CountIfNewline(4, '\n'));
+	REQUIRE(1 == CountIfNewline(0, '\n'));
+	REQUIRE(-2 == CountIfNewline(-3, '\n'));
+
+	REQUIRE(0 == CountIfNewline(0, 'n'));
+	REQUIRE(7 == CountIfNewline(7, 'w'));
+}
+
+TEST_CASE("NewLineInString Accumulate")
+{
+	std::string input {"The\nanswer\nis\nthree"};
+
+	auto result {LinesInString_Accumulate(input)};
+	REQUIRE(3 == result);
+
+	input = "\nthe\nanswoer\nis\nfour";
+	result = LinesInString_Accumulate(input);
+	REQUIRE(4 == result);
 }
