d28532c2ae70d0f824fa01082df86cf7bdcfbf99
Mike Williams <mumakin@gmail.com>
2019-08-18 17:34:25 -0600

    rangev3 accumulate works but not with pipe inline with accumulate

--- chapter02/chapter02.cpp
+++ chapter02/chapter02.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
-#include <fstream>
+#include <functional>
 #include <string>
 #include <vector>
+
+#include <iostream>
 
 #include "range/v3/all.hpp"
 
@@ -32,14 +34,48 @@ TEST_CASE("CountIfNewline")
 	REQUIRE(7 == CountIfNewline(7, 'w'));
 }
 
-TEST_CASE("NewLineInString Accumulate")
+void TestNewlineCountinfFunction(std::function<int(const std::string)> f)
 {
 	std::string input {"The\nanswer\nis\nthree"};
 
-	auto result {LinesInString_Accumulate(input)};
+	auto result {f(input)};
 	REQUIRE(3 == result);
 
 	input = "\nthe\nanswoer\nis\nfour";
-	result = LinesInString_Accumulate(input);
+	result = f(input);
 	REQUIRE(4 == result);
+}
+
+TEST_CASE("NewLineInString Accumulate")
+{
+	TestNewlineCountinfFunction(LinesInString_Accumulate);
+}
+
+int OneIfCharIsNewline(const char c)
+{
+	if('\n' == c)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+TEST_CASE("OneIfCharIsNewline")
+{
+	REQUIRE(0 == OneIfCharIsNewline('n'));
+	REQUIRE(0 == OneIfCharIsNewline('a'));
+	REQUIRE(1 == OneIfCharIsNewline('\n'));
+}
+
+int LinesInString_RangeAccumulate(const std::string input)
+{
+	//return ranges::accumulate(input ! ranges::views::transform(OneIfCharIsNewline), 0);
+	// the above does not compile, but the below, with the intermediate range result does...
+	auto transformed {input | ranges::views::transform(OneIfCharIsNewline)};
+	return ranges::accumulate(transformed, 0);
+}
+
+TEST_CASE("NewLineInString RangeAccumulate")
+{
+	TestNewlineCountinfFunction(LinesInString_RangeAccumulate);
 }
