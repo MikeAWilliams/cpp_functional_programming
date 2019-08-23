60a37cf3b5716dd88b3cc5fe95c6902cfd90f0bb
Mike Williams <mumakin@gmail.com>
2019-08-22 22:00:27 -0600

    add example 2_7

---
+++ chapter02/chapter02_07.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+#include "range/v3/all.hpp"
+
+#include "catch2/catch.hpp"
+
+struct Person
+{
+	std::string Name;
+	bool IsFemale;
+};
+
+bool IsFremale(const Person& person)
+{
+	return person.IsFemale;
+}
+
+bool IsNotFemale(const Person& person)
+{
+	return !person.IsFemale;
+}
+
+std::string Name(const Person& person)
+{
+	return person.Name;
+}
+
+std::vector<std::string> GetNamesOfFemales(std::vector<Person> people)
+{
+	people.erase(std::remove_if(people.begin(), people.end(), IsNotFemale), people.end());
+	std::vector<std::string> result(people.size());
+
+	std::transform(people.begin(), people.end(), result.begin(), Name);
+	return result;
+}
+
+template<typename t>
+void RequireVectorsMatchUnordered(std::vector<t>& first, std::vector<t>& second)
+{
+	REQUIRE(first.size() == second.size());
+	std::sort(first.begin(), first.end());
+	std::sort(second.begin(), second.end());
+	REQUIRE(first == second);
+}
+
+TEST_CASE("GetNamesOfFemalesMixed")
+{
+	std::vector<Person> input {{"Bob", false}, {"Susan", true}, {"Joe", false}, {"Stephanie", true}};
+	std::vector<std::string> expectedResult {"Stephanie", "Susan"};
+
+	auto result {GetNamesOfFemales(input)};
+	RequireVectorsMatchUnordered(expectedResult, result);
+}
