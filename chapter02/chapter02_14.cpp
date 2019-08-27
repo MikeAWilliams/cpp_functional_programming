09a45a6b07443133580ca16ca49592d18608f128
Mike Williams <mumakin@gmail.com>
2019-08-26 22:15:12 -0600

    failing to time tail recursion

--- chapter02/chapter02_14.cpp
+++ chapter02/chapter02_14.cpp
@@ -2,7 +2,7 @@
 #include <functional>
 #include <string>
 #include <vector>
-
+#include <chrono>
 #include <iostream>
 
 #include "catch2/catch.hpp"
@@ -31,7 +31,7 @@ static std::string Name(const Person& person)
 // TODO I am not happy specifying the iterator in the template list. I think it should be std::vector<inputType>::const_iterator
 // however that doesn't compile right now and I am moving on
 template<typename resultType, typename inputType, typename iteratorType>
-std::vector<resultType> FilterTransform(
+std::vector<resultType> FilterTransformTailRecursion(
 	iteratorType dataBegin,
 	iteratorType dataEnd,
 	std::function<bool(const inputType&)> filter,
@@ -48,12 +48,12 @@ std::vector<resultType> FilterTransform(
 	{
 		previousResult.push_back(transform(head));
 	}
-	return FilterTransform(dataBegin + 1, dataEnd, filter, transform, previousResult);
+	return FilterTransformTailRecursion(dataBegin + 1, dataEnd, filter, transform, previousResult);
 }
 
-static std::vector<std::string> GetNamesOfFemales(const std::vector<Person>& people)
+static std::vector<std::string> GetNamesOfFemalesTailRecursion(const std::vector<Person>& people)
 {
-	return FilterTransform<std::string, Person, std::vector<Person>::const_iterator>(people.cbegin(), people.cend(), 
+	return FilterTransformTailRecursion<std::string, Person, std::vector<Person>::const_iterator>(people.cbegin(), people.cend(), 
 	    [](const Person& p)
 		{
 			return IsFemale(p);
@@ -73,29 +73,161 @@ static void RequireVectorsMatchUnordered(std::vector<t>& first, std::vector<t>& 
 	REQUIRE(first == second);
 }
 
-TEST_CASE("GetNamesOfFemalesMixed_14")
+static void RunTest(std::function<std::vector<std::string>(const std::vector<Person>&)> toTest, 
+	const std::vector<Person>& input, std::vector<std::string>& expectedResult)
+{
+	auto result {toTest(input)};
+	RequireVectorsMatchUnordered(expectedResult, result);
+}
+
+static void RunTestMixed(std::function<std::vector<std::string>(const std::vector<Person>&)> toTest)
 {
 	std::vector<Person> input {{"Bob", false}, {"Susan", true}, {"Joe", false}, {"Stephanie", true}};
 	std::vector<std::string> expectedResult {"Stephanie", "Susan"};
-
-	auto result {GetNamesOfFemales(input)};
-	RequireVectorsMatchUnordered(expectedResult, result);
+	
+	RunTest(toTest, input, expectedResult);
 }
 
-TEST_CASE("GetNamesOfFemalesAllMale_14")
+static void RunTestAllMale(std::function<std::vector<std::string>(const std::vector<Person>&)> toTest)
 {
 	std::vector<Person> input {{"Bob", false}, {"Susan", false}, {"Joe", false}, {"Stephanie", false}};
 	std::vector<std::string> expectedResult {};
-
-	auto result {GetNamesOfFemales(input)};
-	RequireVectorsMatchUnordered(expectedResult, result);
+	
+	RunTest(toTest, input, expectedResult);
 }
 
-TEST_CASE("GetNamesOfFemalesAllFemale_14")
+static void RunTestAllFemale(std::function<std::vector<std::string>(const std::vector<Person>&)> toTest)
 {
 	std::vector<Person> input {{"Bob", true}, {"Susan", true}, {"Joe", true}, {"Stephanie", true}};
 	std::vector<std::string> expectedResult {"Bob", "Joe", "Susan", "Stephanie"};
 
-	auto result {GetNamesOfFemales(input)};
-	RequireVectorsMatchUnordered(expectedResult, result);
+	RunTest(toTest, input, expectedResult);
+}
+
+TEST_CASE("GetNamesOfFemalesMixed_TailRecursion")
+{
+	RunTestMixed(GetNamesOfFemalesTailRecursion);
+}
+
+TEST_CASE("GetNamesOfFemalesAllMale_TailRecursion")
+{
+	RunTestAllMale(GetNamesOfFemalesTailRecursion);
+}
+
+TEST_CASE("GetNamesOfFemalesAllFemale_TailRecursion")
+{
+	RunTestAllFemale(GetNamesOfFemalesTailRecursion);
+}
+
+template<typename resultType, typename inputType, typename iteratorType>
+std::vector<resultType> FilterTransformNoTailRecursion(
+	iteratorType dataBegin,
+	iteratorType dataEnd,
+	std::function<bool(const inputType&)> filter,
+	std::function<resultType(const inputType&)> transform,
+	std::vector<resultType> previousResult)
+{
+	if(dataBegin == dataEnd)
+	{
+		return previousResult;
+	}
+	
+	const auto head = *dataBegin;
+	auto result {FilterTransformTailRecursion(dataBegin + 1, dataEnd, filter, transform, previousResult)};
+
+	if(filter(head))
+	{
+		result.push_back(transform(head));
+	}
+	return result;
+}
+
+static std::vector<std::string> GetNamesOfFemalesNoTailRecursion(const std::vector<Person>& people)
+{
+	return FilterTransformNoTailRecursion<std::string, Person, std::vector<Person>::const_iterator>(people.cbegin(), people.cend(), 
+	    [](const Person& p)
+		{
+			return IsFemale(p);
+		},
+		[](const Person& p)
+		{
+			return Name(p); 
+		}, {});
+}
+
+TEST_CASE("GetNamesOfFemalesMixed_NoTailRecursion")
+{
+	RunTestMixed(GetNamesOfFemalesNoTailRecursion);
+}
+
+TEST_CASE("GetNamesOfFemalesAllMale_NoTailRecursion")
+{
+	RunTestAllMale(GetNamesOfFemalesNoTailRecursion);
+}
+
+TEST_CASE("GetNamesOfFemalesAllFemale_NoTailRecursion")
+{
+	RunTestAllFemale(GetNamesOfFemalesNoTailRecursion);
+}
+
+// now that I have two methods one with tail recursion and the other without lets see if we can measure the performance difference
+
+struct TestSet
+{
+	std::vector<Person> input;
+	std::vector<std::string> expectedResult;
+};
+
+TestSet GenerateLargeTestSet()
+{
+	const size_t input_size {10'000};
+	TestSet result {std::vector<Person>(input_size), std::vector<std::string>(input_size / 2)};
+	
+	bool female {true};
+	auto expectedIter = result.expectedResult.begin();
+	size_t i {0};
+	for(auto & person : result.input)
+	{
+		person.Name = std::to_string(i);
+		person.IsFemale = female;
+		if(female)
+		{
+			*expectedIter = person.Name;
+			++expectedIter;
+		}
+		female = !female;
+		++i;
+	}
+	
+	return result;
+}
+
+TEST_CASE("Time tail recursion")
+{
+	auto set {GenerateLargeTestSet()};
+	auto start {std::chrono::high_resolution_clock::now()};
+
+	for(int i = 0; i < 100; ++i)
+	{
+		RunTest(GetNamesOfFemalesTailRecursion, set.input, set.expectedResult);
+	}
+	
+	auto end {std::chrono::high_resolution_clock::now()};
+	std::chrono::duration<double> time {end - start};
+	std::cout << "tail reucrsion time " << time.count() << std::endl;;
+}
+
+TEST_CASE("Time no tail recursion")
+{
+	auto set {GenerateLargeTestSet()};
+	auto start {std::chrono::high_resolution_clock::now()};
+	
+	for(int i = 0; i < 100; ++i)
+	{
+		RunTest(GetNamesOfFemalesNoTailRecursion, set.input, set.expectedResult);
+	}
+	
+	auto end {std::chrono::high_resolution_clock::now()};
+	std::chrono::duration<double> time {end - start};
+	std::cout << "no tail reucrsion time " << time.count() << std::endl;
 }
