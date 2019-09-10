4de7bcfd7bd58847c6fa7e44ebb11aedbfd7de34
Mike Williams <mumakin@gmail.com>
2019-09-09 23:23:37 -0600

    experimenting with currying

---
+++ chapter04/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cmake -G "Unix Makefiles" 
+cmake --build . --config Release 
+./chapter04
---
+++ chapter04/currying_performance.cpp
@@ -0,0 +1,59 @@
+#include <exception>
+#include <functional>
+#include <iostream>
+
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include "catch2/catch.hpp"
+
+double MultiplyThreeNumbers(double one, double two, double three)
+{
+	//std::cout << one << " " << two << " " << three << std::endl;
+	return one * two * three;
+}
+
+std::function<double(double)> MultiplyThreeNumbers(double one, double two)
+{
+	return [one = one, two = two](double three)
+	{
+		return MultiplyThreeNumbers(one, two, three);
+	};
+}
+
+std::function<std::function<double(double)>(double)> MultiplyThreeNumbers(double one)
+{
+	return [one = one](double two)
+		{
+			return MultiplyThreeNumbers(one, two);
+		};
+}
+
+
+TEST_CASE("Currying performance testing")
+{
+	REQUIRE(24 == MultiplyThreeNumbers(2, 3, 4));
+	REQUIRE(24 == MultiplyThreeNumbers(2)(3)(4));
+
+	BENCHMARK("Call three arg")
+	{	
+		for(double i = 2; i < 10; ++i)
+		{
+			MultiplyThreeNumbers(i, i+1, i+2);
+		}
+	};
+
+	BENCHMARK("Call two arg")
+	{	
+		for(double i = 2; i < 10; ++i)
+		{
+			MultiplyThreeNumbers(i, i+1)(i+2);
+		}
+	};
+
+	BENCHMARK("Call one arg")
+	{	
+		for(double i = 2; i < 10; ++i)
+		{
+			MultiplyThreeNumbers(i)(i+1)(i+2);
+		}
+	};
+}
