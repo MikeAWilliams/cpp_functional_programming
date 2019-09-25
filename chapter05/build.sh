614561f5c4d9babdcc333340a4d88969478718bc
MikeAWilliams <mumakin@gmail.com>
2019-09-24 21:51:47 -0600

    fix chapter 5

--- chapter05/build.sh
+++ chapter05/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cmake -G "Unix Makefiles" 
-cmake --build . --config Release 
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=release "-DCMAKE_TOOLCHAIN_FILE=/mnt/d/data/programming/cpp/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake --build .
 ./chapter05
