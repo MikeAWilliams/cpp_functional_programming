88dfad76e933fcba579fd03f1f2e1dfd7f76440d
MikeAWilliams <mumakin@gmail.com>
2019-09-24 21:35:54 -0600

    fix chapter1 cmake

--- chapter01/build.sh
+++ chapter01/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-make -f ./Makefile
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=release "-DCMAKE_TOOLCHAIN_FILE=/mnt/d/data/programming/cpp/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake --build .
 ./chapter01
