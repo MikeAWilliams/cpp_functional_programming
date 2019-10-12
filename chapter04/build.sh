e9c857b89ae91f0f6f297b726598c7350f5ae723
MikeAWilliams <mumakin@gmail.com>
2019-10-12 08:02:07 -0600

    switch chapter04 to ~ for home

--- chapter04/build.sh
+++ chapter04/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=release "-DCMAKE_TOOLCHAIN_FILE=/mnt/d/data/programming/cpp/vcpkg/scripts/buildsystems/vcpkg.cmake"
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=release "-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake"
 cmake --build .
 ./chapter04
