2a8836b75dde4a48ca68d3b6d5afa89053e9191f
MikeAWilliams <mumakin@gmail.com>
2020-05-30 09:03:56 -0600

    ranges and immer

--- chapter08/helloImmer.cpp
+++ chapter08/helloImmer.cpp
@@ -1,5 +1,6 @@
 #include "catch2/catch.hpp"
 #include "immer/array.hpp"
+#include "range/v3/all.hpp"
 
 TEST_CASE("first array test", "[immer]")
 {
@@ -8,4 +9,15 @@ TEST_CASE("first array test", "[immer]")
 
     REQUIRE((v1 == immer::array<int>{1}));
     REQUIRE((v2 == immer::array<int>{1, 8}));
+}
+
+auto is_six = [](int i) { return i == 6; };
+
+TEST_CASE("range example with immer array", "[immer]")
+{
+    immer::array<int> v{6, 2, 3, 4, 5, 6};
+ 
+    REQUIRE(ranges::any_of(v, is_six));
+    REQUIRE_FALSE(ranges::all_of(v, is_six));
+    REQUIRE_FALSE(ranges::none_of(v, is_six));
 }
