02d837680bd30246cbc3dbc3cc6dc83026fa8fab
MikeAWilliams <mumakin@gmail.com>
2020-06-08 20:15:29 -0600

    first passing test:
    uit
    ~
    
    :

--- chapter09/tennis.cpp
+++ chapter09/tennis.cpp
@@ -1,6 +1,73 @@
 #include "catch2/catch.hpp"
 
-TEST_CASE("stupid", "[tennis]")
+#include <tuple>
+#include <variant>
+
+enum class TennisScore
 {
-    REQUIRE(false);
+    love,
+    fifteen,
+    thirty,
+    fourty,
+    advantage,
+    notAdvantage,
+    victory
+};
+
+enum class player
+{
+    player1,
+    player2
+};
+
+class TennisGame
+{
+    enum class points 
+    {
+        love,
+        fifteen,
+        thirty
+    };
+
+    struct normalScoring
+    {
+        points player1Score;
+        points player2Score;
+    };
+
+    struct fourtyScoring
+    {
+        player leader;
+        points other_player_score;
+    };
+
+    struct duce {};
+
+    struct advantageScoring
+    {
+        player whoHasAdvantate;
+    };
+
+    std::variant<normalScoring, fourtyScoring, duce, advantageScoring> m_state;
+
+    public:
+    TennisGame()
+        : m_state{normalScoring{ points::love, points::love}}
+    {
+
+    }
+
+    std::tuple<TennisScore, TennisScore> GetScore()
+    {
+        return {TennisScore::love, TennisScore::love};
+    }
+};
+
+TEST_CASE("test constructor", "[tennis]")
+{
+    TennisGame testObject;
+    auto [ p1Score, p2Score] = testObject.GetScore();
+
+    REQUIRE(TennisScore::love == p1Score);
+    REQUIRE(TennisScore::love == p2Score);
 }
