7e81c83513e929c31a0817b2f118db620db129d9
MikeAWilliams <mumakin@gmail.com>
2020-06-08 20:36:14 -0600

    pattern matching in place

--- chapter09/tennis.cpp
+++ chapter09/tennis.cpp
@@ -1,7 +1,14 @@
 #include "catch2/catch.hpp"
 
+#include <iostream>
 #include <tuple>
 #include <variant>
+
+// This is an example sketched out in Functional Programming in C++ by Ivan Cukic chapter 9 section 9.3
+// The pattern matcher template is the authors with out understanding or modification from me
+template <typename... Ts>
+struct patternMatcher : Ts... { using Ts::operator()...;};
+template <typename... Ts> patternMatcher(Ts...) -> patternMatcher<Ts...>;
 
 enum class TennisScore
 {
@@ -14,7 +21,7 @@ enum class TennisScore
     victory
 };
 
-enum class player
+enum class Player
 {
     player1,
     player2
@@ -37,15 +44,15 @@ class TennisGame
 
     struct fourtyScoring
     {
-        player leader;
-        points other_player_score;
+        Player leader;
+        points otherPlayerScore;
     };
 
     struct duce {};
 
     struct advantageScoring
     {
-        player whoHasAdvantate;
+        Player whoHasAdvantate;
     };
 
     std::variant<normalScoring, fourtyScoring, duce, advantageScoring> m_state;
@@ -54,6 +61,33 @@ class TennisGame
     TennisGame()
         : m_state{normalScoring{ points::love, points::love}}
     {
+
+    }
+
+    void ScorePoint(Player whoScoredThePoint)
+    {
+        std::visit(
+            patternMatcher 
+            {
+                [&](const normalScoring state)
+                {
+                    std::cout << "scoring point in normal scoring" << std::endl;
+                },
+                [&](const fourtyScoring state)
+                {
+                    std::cout << "scoring point in fourty scoring" << std::endl;
+                },
+                [&](const duce state)
+                {
+                    std::cout << "scoring point in douce scoring" << std::endl;
+                },
+                [&](const advantageScoring state)
+                {
+                    std::cout << "scoring point in advantage scoring" << std::endl;
+                }
+
+            },
+            m_state);
 
     }
 
@@ -69,5 +103,16 @@ TEST_CASE("test constructor", "[tennis]")
     auto [ p1Score, p2Score] = testObject.GetScore();
 
     REQUIRE(TennisScore::love == p1Score);
+    REQUIRE(TennisScore::love == p2Score);
+}
+
+TEST_CASE("test scoring one point", "[tennis]")
+{
+    TennisGame testObject;
+    
+    testObject.ScorePoint(Player::player1);
+    auto [ p1Score, p2Score] = testObject.GetScore();
+
+    REQUIRE(TennisScore::fifteen == p1Score);
     REQUIRE(TennisScore::love == p2Score);
 }
