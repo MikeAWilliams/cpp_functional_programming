af6581f819e98239690b07e4187d6d89d652577f
Mike Williams <mumakin@gmail.com>
2020-06-22 09:04:15 -0600

    optional monad in place

--- chapter10/optional_scratch.cpp
+++ chapter10/optional_scratch.cpp
@@ -14,6 +14,16 @@ auto transform(const std::optional<T1>& opt, F f) -> decltype(std::make_optional
    return {};
 }
 
+template <typename T1, typename F>
+auto mbind(const std::optional<T1>& opt, F f) -> decltype(f(opt.value()))
+{
+   if(opt)
+   {
+      return f(opt.value());
+   }
+   return {};
+}
+
 double Add3(double value)
 {
    return value + 3;
@@ -28,4 +38,45 @@ TEST_CASE("test transrom", "[optional_scratch]")
    auto resultWithTwo = transform(optional2, Add3);
    REQUIRE(resultWithTwo);
    REQUIRE(resultWithTwo.value() == 5.0);
+}
+
+bool IsEven(int value)
+{
+   return value % 2 == 0;
+}
+
+TEST_CASE("IsEven", "[optional_scratch]")
+{
+   REQUIRE(IsEven(4));
+   REQUIRE(IsEven(0));
+   REQUIRE_FALSE(IsEven(3));
+}
+
+std::optional<int> Half(int value)
+{
+   if(IsEven(value))
+   {
+      return value / 2;
+   }
+   return {};
+}
+
+TEST_CASE("half", "[optional_scratch]")
+{
+  auto fourResult {Half(4)};
+  REQUIRE(fourResult);
+  REQUIRE(fourResult.value() == 2);
+
+  auto threeResult {Half(3)};
+  REQUIRE_FALSE(threeResult);
+}
+
+TEST_CASE("test mbind", "[optional_scratch]")
+{
+   auto fourResult {mbind(std::optional<int>{4}, Half)};
+   REQUIRE(fourResult);
+   REQUIRE(fourResult.value() == 2);
+
+   auto threeResult {mbind(std::optional<int>{3}, Half)};
+   REQUIRE_FALSE(threeResult);
 }
